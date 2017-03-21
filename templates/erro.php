bacb303bada4b5f4cbf3887f60a556f186e2a3ba
Kubinyete <vitorkubinyete@hotmail.com>
2017-03-21 11:06:16 -0300

    Alterado o design da caixa de descrição da imagem

--- templates/erro.php
+++ templates/erro.php
@@ -1,5 +1,7 @@
 <main>
-	<h1><?php echo $erroTitulo; ?></h1>
-	<p><?php echo $erroDescricao; ?></p>
-	<p class="erro"><?php echo $erroAbrev; ?></p>
+	<article>
+		<h1><?php echo $erroTitulo; ?></h1>
+		<p><?php echo $erroDescricao; ?></p>
+		<p class="erro"><?php echo $erroAbrev; ?></p>
+	</article>
 </main>
--- templates/home.php
+++ templates/home.php
@@ -1,26 +1,28 @@
 <main>
-	<h1>Adicionadas recentemente</h1>
-	<p>Estas são as imagens recentemente adicionadas até o momento!</p>
-	<?php if (count($imagens) >= 1) { ?>
-	<ul class="imagens-lista"><!--
-		<?php foreach ($imagens as $imagem) { ?>
-		--><li>
-			<a href="<?php echo $imagem->getLink(); ?>">
-				<div class="imagem-container">
-					<div style="background-image: url('<?php echo $imagem->getImagemUrl(); ?>')" class="imagem"></div>
-					<div class="imagem-gradient"></div>
-				</div>
-				<span class="imagem-titulo">
-					<?php echo $imagem->getTitulo(true); ?>
+	<article>
+		<h1>Adicionadas recentemente</h1>
+		<p>Estas são as imagens recentemente adicionadas até o momento!</p>
+		<?php if (count($imagens) >= 1) { ?>
+		<ul class="imagens-lista"><!--
+			<?php foreach ($imagens as $imagem) { ?>
+			--><li>
+				<a href="<?php echo $imagem->getLink(); ?>">
+					<div class="imagem-container">
+						<div style="background-image: url('<?php echo $imagem->getImagemUrl(); ?>')" class="imagem"></div>
+						<div class="imagem-gradient"></div>
+					</div>
+					<span class="imagem-titulo">
+						<?php echo $imagem->getTitulo(true); ?>
+					</span>
+				</a>
+				<span class="imagem-descricao">
+					<?php echo $imagem->getDescricao(true); ?>
 				</span>
-			</a>
-			<span class="imagem-descricao">
-				<?php echo $imagem->getDescricao(true); ?>
-			</span>
-		</li><!-- 
+			</li><!-- 
+			<?php } ?>
+		--></ul>
+		<?php } else { ?>
+		<p class="erro">Nenhuma imagem encontrada no banco de dados :(.</p>
 		<?php } ?>
-	--></ul>
-	<?php } else { ?>
-	<p class="erro">Nenhuma imagem encontrada no banco de dados :(.</p>
-	<?php } ?>
+	</article>
 </main>

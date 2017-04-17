88636575fa4901b275fc6ea7bd241d5614aa37bc
Kubinyete <vitorkubinyete@hotmail.com>
2017-04-17 01:19:40 -0300

    Erro -> Aviso

--- templates/erro_dialogo.php
+++ templates/erro_dialogo.php
@@ -1,7 +1,7 @@
 <?php if ($itens["erro_dialogo"] !== null && strlen($itens["erro_dialogo"]) > 0): ?>
 <div class='erro-dialogo-fundo'>
 	<div class='erro-dialogo'>
-		<h1>Erro</h1>
+		<h1>Aviso</h1>
 		<p><?= $itens["erro_dialogo"]; ?></p>
 		<button onclick="phpgallery.erroDialogo.desativar();">Ok</button>
 	</div>
